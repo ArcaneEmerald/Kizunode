7422323b84e7e7b2706edde30d465daafc2bd930
ArcaneEmerald <kehangl@student.unimelb.edu.au>
2026-03-22 23:01:43 +1100

    complete relationships backend

--- server/Persistence/Migrations/20260322052111_ActivityAttendeesAdded.Designer.cs
+++ server/Persistence/Migrations/20260322052111_ActivityAttendeesAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
@@ -10,9 +11,11 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260322052111_ActivityAttendeesAdded")]
+    partial class ActivityAttendeesAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.13");
@@ -37,7 +40,7 @@ namespace Persistence.Migrations
                         .IsRequired()
                         .HasColumnType("TEXT");
 
-                    b.Property<bool>("IsCancelled")
+                    b.Property<bool>("IsCanceled")
                         .HasColumnType("INTEGER");
 
                     b.Property<double>("Latitude")
