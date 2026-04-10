d25d55a0d78dfa62355c67f1d05e9ccfd3c6b3ad
ArcaneEmerald <kehangl@student.unimelb.edu.au>
2026-04-10 19:22:17 +1000

    complete image upload backend

--- server/API/Program.cs
+++ server/API/Program.cs
@@ -5,6 +5,7 @@ using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using Infrastructure;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -32,6 +33,7 @@ builder.Services.AddMediatR(x =>
 });
 
 builder.Services.AddScoped<IUserAccessor, UserAccessor>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptionMiddleware>();
@@ -43,6 +45,7 @@ builder.Services.AddAuthorization(opt =>
     opt.AddPolicy("IsActivityHost", policy => { policy.Requirements.Add(new IsHostRequirement()); });
 });
 builder.Services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 
 var app = builder.Build();
 
--- server/Infrastructure/Security/UserAccessor.cs
+++ server/Infrastructure/Security/UserAccessor.cs
@@ -2,6 +2,7 @@ using System.Security.Claims;
 using Application.Interfaces;
 using Domain;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Infrastructure;
@@ -18,5 +19,15 @@ public class UserAccessor(IHttpContextAccessor httpContextAccessor, AppDbContext
     {
         return await dbContext.Users.FindAsync(GetUserId())
                ?? throw new UnauthorizedAccessException("No user is logged in");
+    }
+
+    public async Task<User> GetUserWithPhotosAsync()
+    {
+        var userId = GetUserId();
+
+        return await dbContext.Users
+                   .Include(x => x.Photos)
+                   .FirstOrDefaultAsync(x => x.Id == userId)
+               ?? throw new UnauthorizedAccessException("Cannot find user in the DB");
     }
 }
